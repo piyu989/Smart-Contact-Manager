578c5a5bbcb77a9df64815ef37d3cb37b3c2b147
piyu989 <sab9861@gmail.com>
2024-10-20 23:45:22 +0530

    ..

--- Smart-CM/src/main/java/com/cm/helper/Helper.java
+++ Smart-CM/src/main/java/com/cm/helper/Helper.java
@@ -4,28 +4,27 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import lombok.var;
-
 public class Helper {
-    public static String getEmailOfLoggedInUser(Authentication authentication){
+    public static String getEmailOfLoggedInUser(Authentication authentication) {
 
-        if(authentication instanceof OAuth2AuthenticationToken){
+        if (authentication instanceof OAuth2AuthenticationToken) {
 
-            var oAuth2AuthenticationToken =(OAuth2AuthenticationToken)authentication;
-            var clientId=oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+            // Explicitly define the types instead of using var
+            OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+            String clientId = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
 
-            var oauth2User=(OAuth2User)authentication.getPrincipal();
+            OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
 
-            if(clientId.equalsIgnoreCase("google")){
+            if (clientId.equalsIgnoreCase("google")) {
                 System.out.println("Getting email from google");
                 return oauth2User.getAttribute("email");
-            }else if(clientId.equalsIgnoreCase("github")){
+            } else if (clientId.equalsIgnoreCase("github")) {
                 System.out.println("get email using github");
-                return oauth2User.getAttribute("email")!=null?oauth2User.getAttribute("email"):oauth2User.getAttribute("login").toString()+"@gmail.com";
+                return oauth2User.getAttribute("email") != null ? oauth2User.getAttribute("email") : oauth2User.getAttribute("login").toString() + "@gmail.com";
             }
-            
+
             return "";
-        }else{
+        } else {
             System.out.println("get email manually");
             return authentication.getName();
         }
